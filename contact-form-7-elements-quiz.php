a35039bbf12058e3d40b2f7d94557312ad91529e
Jesus Magallon <j.magallon@oohmd.com>
2018-01-01 22:26:06 -0600

    First commit

---
+++ contact-form-7-elements-quiz.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Contact Form 7 Elements Quiz
+Plugin URI: 
+Description: Plugin created to add support to Contact Form 7 to perform element tests.
+Version: 0.1
+Author: Jesús Magallón
+Author URI: http://yosoydev.net
+License: GNU GPLv3
+License URI: https://choosealicense.com/licenses/gpl-3.0/
+*/
